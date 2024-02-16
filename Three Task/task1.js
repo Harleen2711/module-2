b7f18458011b81d15a5047dbf66f8ce07fc74fb0
Harleen <155694034+Harleen2711@users.noreply.github.com>
2024-02-15 18:04:15 -0700

    done

---
+++ Three Task/task1.js
@@ -0,0 +1,8 @@
+var firstName = "Moe", lastName = "Adam", age = 52;
+
+      
+      var firstname = "Mike";
+      var lastname = "Douglas";
+      var fullname = firstname + " " + lastname;
+
+      console.log(fullname);
